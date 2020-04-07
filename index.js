54bd4cc37a0959174f1fe8db7eb4b397318ffa59
Behnam Sheykhe <learndlt@gmail.com>
2020-04-07 16:06:29 +0430

    add chalk fore colors and bold folders and add nls .. for last dir

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 const util = require('util');
+const chalk = require('chalk');
+const path = require('path');
 
 // Method #2
 // const lstat = util.promisify(fs.lstat);
@@ -9,13 +11,15 @@ const util = require('util');
 // Method #3
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
   const statPromises = filenames.map(filename => {
-    return lstat(filename);
+    return lstat(path.join(targetDir, filename));
   });
 
   const allStats = await Promise.all(statPromises);
@@ -23,7 +27,11 @@ fs.readdir(process.cwd(), async (err, filenames) => {
   for (let stats of allStats) {
     const index = allStats.indexOf(stats);
 
-    console.log(filenames[index], stats.isFile());
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.bold(filenames[index]));
+    }
   }
 });
 
